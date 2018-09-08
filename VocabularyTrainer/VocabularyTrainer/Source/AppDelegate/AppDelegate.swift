2a494e5a381622865d630f17cac3b42db047b3ba
pavel <pavel.antonov1980@icloud.com>
2018-09-08 11:33:30 +0300

    base project added

---
+++ VocabularyTrainer/VocabularyTrainer/Source/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  VocabularyTrainer
+//
+//  Created by Antonov, Pavel on 9/8/18.
+//  Copyright © 2018 Pavel Antonov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
